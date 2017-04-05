ca4a9bdda7c688a9c078b5e2e61ce8f8fd24b6a6
Luciano Resende <lresende@apache.org>
2017-04-05 09:42:07 -0700

    Support installing kerberos on multi-node cluster

--- kdc.sh
+++ kdc.sh
@@ -18,7 +18,21 @@
 ROOT=`dirname $0`
 ROOT=`cd $ROOT; pwd`
 
+HOSTS=(lresende-iop-2.fyre.ibm.com lresende-iop-3.fyre.ibm.com lresende-iop-4.fyre.ibm.com lresende-iop-5.fyre.ibm.com)
 LOCALHOST="$(/bin/hostname -f)"
+
+CLUSTER_MASTER=${HOSTS[0]}
+CLUSTER_NODES=${HOSTS[@]:1}
+CLUSTER_SIZE=${#HOSTS[@]}
+NODES=("${HOSTS[@]:1}") ##Workaround to get node size
+CLUSTER_NODE_SIZE=${#NODES[@]}
+
+echo ">>> Cluster Configuration "
+echo "Cluster Nodes.: ${HOSTS[@]}"
+echo "Master Node...: $CLUSTER_MASTER"
+echo "Data Nodes....: $CLUSTER_NODES"
+echo "Cluster size..: $CLUSTER_NODE_SIZE"
+echo ">>> "
 
 REALM="IBM.COM"
 
@@ -55,3 +69,13 @@ kadmin.local -q "addprinc admin/admin"
 
 echo -e "\n Restarting kadmin"
 service kadmin restart
+
+
+for i in ${CLUSTER_NODES[@]}; do
+  echo -e "\n Configuring Kerberos client at ${i}"
+  yum -y install krb5-libs krb5-workstation
+
+  ssh -o StrictHostKeyChecking=no root@${i} "if [ ! -f /etc/krb5.conf.backup ]; then cp /etc/krb5.conf /etc/krb5.conf.backup; fi"
+  cat etc/krb5.conf | ssh -o StrictHostKeyChecking=no root@${i} "cat > /etc/krb5.conf"
+  ssh -o StrictHostKeyChecking=no root@${i} "sed -i.bak \"s/kerberos.example.com/$CLUSTER_MASTER/g\" /etc/krb5.conf"
+done
